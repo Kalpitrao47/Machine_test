c5f5fa80b87554c4f25f27c0c3909e0344275a0a
unknown <kalpitraorane47@gmail.com>
2024-01-11 17:13:26 +0530

    TopProfile

--- src/Components/TopProfile.js
+++ src/Components/TopProfile.js
@@ -5,9 +5,21 @@ const TopProfile = () => {
     <div className="mt-10">
       <h1 className="font-semibold text-4xl">Top Profile For This Week</h1>
       <div>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div className="flex flex-col">
+          <img src="https://www.shutterstock.com/image-photo/portrait-attractive-cheerful-girl-demonstrating-260nw-2113649489.jpg" />
+          <h2>Amita Patil</h2>
+          <h3>35K</h3>
+        </div>
+        <div className="flex flex-col">
+          <img src="https://www.shutterstock.com/image-photo/portrait-attractive-cheerful-girl-demonstrating-260nw-2113649489.jpg" />
+          <h2>Nita Choudhari</h2>
+          <h3>99K</h3>
+        </div>
+        <div className="flex flex-col">
+          <img src="https://www.shutterstock.com/image-photo/portrait-attractive-cheerful-girl-demonstrating-260nw-2113649489.jpg" />
+          <h2>Anisha Roy</h2>
+          <h3>25K</h3>
+        </div>
       </div>
     </div>
   );
