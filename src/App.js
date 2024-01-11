09e415ec9d0500df942ee3f93c01ce800e1c02c3
unknown <kalpitraorane47@gmail.com>
2024-01-11 16:49:41 +0530

    SocialLogo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header';
+import GradientAbout from './Components/GradientAbout';
+import SocialLogo from './Components/SocialLogo';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <GradientAbout/>
+      <SocialLogo/>
     </div>
   );
 }
---
+++ src/Components/SocialLogo.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const SocialLogo = () => {
+  return (
+    <div>
+        <h1>
+            Instagram
+        </h1>
+        <h1>
+            Facebook
+        </h1>
+        <h1>
+            Youtube
+        </h1>
+        <h1>
+            Twitter
+        </h1>
+    </div>
+  )
+}
+
+export default SocialLogo
